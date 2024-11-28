6330024078533ad119decd6148da5f35a46a01d3
anyuegonzalez <anyuee07@gmail.com>
2024-11-28 21:15:09 +0100

    a

--- Assets/Scripts/Cofre.cs
+++ Assets/Scripts/Cofre.cs
@@ -6,6 +6,10 @@ public class Cofre : MonoBehaviour
 {
 
     private Outline outline;
+
+    [SerializeField] private Texture2D cursorInteraccion;
+    [SerializeField] private Texture2D cursorPorDefecto;
+
     private void Awake()
     {
         outline = GetComponent<Outline>();
@@ -18,15 +22,18 @@ public class Cofre : MonoBehaviour
     {
         
     }
+
+
     // cuando pasamos el raton por encima
     private void OnMouseEnter()
     {
-        
+        Cursor.SetCursor(cursorInteraccion, Vector2.zero, CursorMode.Auto);
         outline.enabled = true;
     }
     // cuando quitamos el raton por encima
     private void OnMouseExit()
     {
+        Cursor.SetCursor(cursorPorDefecto, Vector2.zero, CursorMode.Auto);
         outline.enabled= false;
     }
 }
