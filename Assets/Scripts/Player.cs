439720fa9bdbe2b88dbd9471709c866d1d373ab7
anyuegonzalez <anyuee07@gmail.com>
2024-12-04 20:55:19 +0100

    m

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -32,6 +32,7 @@ public class Player : MonoBehaviour
             {
                 Debug.Log("Hola!");
                 npcActual = null;
+                agent.isStopped = true;
                 agent.stoppingDistance = 0;
             }
         }
