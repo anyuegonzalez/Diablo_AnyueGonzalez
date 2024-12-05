53b872ae9e7ec0f30d1b4aea31c860a122e43f05
anyuegonzalez <anyuee07@gmail.com>
2024-12-05 21:01:28 +0100

    a

--- Assets/Scripts/Npc.cs
+++ Assets/Scripts/Npc.cs
@@ -16,8 +16,8 @@ public class Npc : MonoBehaviour
     // para que el npc se gire hacia el que le esta interactuando (jugador)
     public void Interactuar(Transform interactuador)
     {
-        Debug.Log("Hola!");
-        transform.DOLookAt(interactuador.position, tiempoRotacion, AxisConstraint.Y);
+        transform.DOLookAt(interactuador.position, tiempoRotacion, AxisConstraint.Y).OnComplete(()=> SistemaDialogo.trono.IniciarDialogo());
+        SistemaDialogo.trono.IniciarDialogo();
     }
     private void OnMouseEnter()
     {  
--- Assets/Scripts/SistemaDialogo.cs
+++ Assets/Scripts/SistemaDialogo.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SistemaDialogo : MonoBehaviour
 {
     public static SistemaDialogo trono; // varios candidatos, el primero que llega se lo encuentra vacio y dice y el trono pa quien? pa mi, me quedo como un rey, si viene otro tipo me mato porque soy un rey falso
+
+    [SerializeField] private GameObject marcos;
+    [SerializeField] private TMP_Text textoDialogo;
+
+    private bool escribiendo; // determina si el sistema esta escribiendo o no 
+    private int indiceFraseActual; // marca la frase por la que voy 
     private void Awake()
     {
         // si el trono esta vacio
@@ -22,14 +29,23 @@ public class SistemaDialogo : MonoBehaviour
         }
 
     }
-    void Start()
-    {
-        
-    }
 
-   
-    void Update()
+    public void IniciarDialogo()
     {
-        
+        marcos.SetActive(true);  
     }
+    // que el texto aparezca letra a letra
+    private void EscribirFrase()
+    {
+
+    }
+    private void SiguienteFrase()
+    {
+
+    }
+    private void TerminarDialogo()
+    { 
+    
+    }
+   
 }
