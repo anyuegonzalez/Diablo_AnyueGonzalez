8ff55f6bfcf66ba24d77a7c27a9bbb7643101c6b
anyuegonzalez <anyuee07@gmail.com>
2024-12-12 21:02:54 +0100

    s

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -1,23 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Enemigo : MonoBehaviour
 {
     [SerializeField] private Transform ruta;
 
-    List<Transform> listadoPuntos = new List<Transform>(); 
+    private NavMeshAgent agent;
+
+    List<Vector3> listadoPuntos = new List<Vector3>();
+
+    private Vector3 destinoActual; // marca el destino actual al cual tenemos que ir
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        // voy recorriendo todos los puntos que tine mi ruta
+        foreach (Transform punto in ruta)
+        {
+            // y los añado en mi lista
+            listadoPuntos.Add(punto.position);
+        }
+        CalcularDestino();
+    }
     void Start()
     {
-        // voy recorriendo todos los puntos que tine mi ruta
-       foreach (Transform punto in ruta)
-       {
-            // y los añado en mi lista
-           listadoPuntos.Add(punto);
-       }
+        StartCoroutine(PatrullarYEsperar());
     }
-    void Update()
+
+    private IEnumerator PatrullarYEsperar()
     {
-        
+        agent.SetDestination(destinoActual);
+        yield return null;
     }
-}
+    private void CalcularDestino()
+    {
+        destinoActual = listadoPuntos[0];
+    }
+}    
+
