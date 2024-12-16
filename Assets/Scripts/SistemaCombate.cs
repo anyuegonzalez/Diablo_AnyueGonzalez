c447cae9e09f2c3b43d126faf7c80ab70be10537
anyuegonzalez <anyuee07@gmail.com>
2024-12-16 18:18:22 +0100

    a

---
+++ Assets/Scripts/SistemaCombate.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SistemaCombate : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
