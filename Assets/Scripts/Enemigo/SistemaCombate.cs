815289f461048e7469e6ffd29da091b3f7484ed8
DESKTOP-GV16N45\Alumno <anyuee07@gmail.com>
2025-01-08 20:28:46 +0100

    saw

--- Assets/Scripts/Enemigo/SistemaCombate.cs
+++ Assets/Scripts/Enemigo/SistemaCombate.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class SistemaCombate : MonoBehaviour
 {
-    
+    [SerializeField] private Enemigo main;
     void Start()
     {
-        
+        main.Combate = this;
     }
     void Update()
     {
