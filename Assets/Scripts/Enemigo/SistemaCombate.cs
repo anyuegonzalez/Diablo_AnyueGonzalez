d2506425f41e55fb7ea77d65ba131835ae4bae0a
DESKTOP-GV16N45\Alumno <anyuee07@gmail.com>
2025-01-08 21:18:07 +0100

    a

--- Assets/Scripts/Enemigo/SistemaCombate.cs
+++ Assets/Scripts/Enemigo/SistemaCombate.cs
@@ -7,6 +7,7 @@ public class SistemaCombate : MonoBehaviour
 {
     [SerializeField] private Enemigo main;
     [SerializeField] private float velocidadCombate;
+    [SerializeField] private float distanciaAtaque;
     [SerializeField] private NavMeshAgent agent;
 
 
@@ -14,13 +15,20 @@ public class SistemaCombate : MonoBehaviour
     {
         main.Combate = this;
     }
+    // se ejecuta despues del awake y solo cuando el script es habilitado, se puede ejecutar varias veces, cada vez que lo habilitas
+    // el combate ha sido habilitadi 
+    private void OnEnable()
+    {
+        agent.speed = velocidadCombate;
+        agent.stoppingDistance = distanciaAtaque;
+    }
+
     void Start()
     {
        
     }
     void Update()
-    {
-        agent.speed = velocidadCombate;
+    {    
         agent.SetDestination(main.MainTarget.position);
     }
 }
