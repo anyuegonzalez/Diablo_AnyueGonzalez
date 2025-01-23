9e27e99ee10adc0bdc3046cc2588280306d38d05
DESKTOP-GV16N45\Alumno <anyuee07@gmail.com>
2025-01-23 21:11:31 +0100

    gf

--- Assets/Scripts/SistemaMisiones.cs
+++ Assets/Scripts/SistemaMisiones.cs
@@ -6,9 +6,15 @@ public class SistemaMisiones : MonoBehaviour
 {
     [SerializeField] private EventManagerSO eventManager;
 
-    private void OnEnable()
+    [SerializeField] private GameObject toggleMision;
+
+    private void OnEnable() 
     {
-        
+        eventManager.OnNuevaMision += ActivarToggleMision; // te suscribes al evento porque me interesa para enterarme de que hay una nueva mision
     }
 
+    private void ActivarToggleMision()
+    {
+       toggleMision.SetActive(true);
+    }
 }
