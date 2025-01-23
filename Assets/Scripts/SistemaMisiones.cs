13ba2d440cafee23605159b4aba5132aa46fcd18
DESKTOP-GV16N45\Alumno <anyuee07@gmail.com>
2025-01-23 21:04:14 +0100

    aa

---
+++ Assets/Scripts/SistemaMisiones.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SistemaMisiones : MonoBehaviour
+{
+    [SerializeField] private EventManagerSO eventManager;
+
+    private void OnEnable()
+    {
+        
+    }
+
+}
