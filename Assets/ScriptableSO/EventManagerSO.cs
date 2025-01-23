5ec6dda34f37ac5782807f396930a21f1fb26ca0
DESKTOP-GV16N45\Alumno <anyuee07@gmail.com>
2025-01-23 21:01:40 +0100

    AA

--- Assets/ScriptableSO/EventManagerSO.cs
+++ Assets/ScriptableSO/EventManagerSO.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Event manager")]
 public class EventManagerSO : ScriptableObject
 {
+    // los metodos que empiezan por On se ejecutan por metodos, de forma automatica
+    public event Action OnNuevaMision;
     public void NuevaMision()
     {
-        
+        // aqui lanzo la notificacion (el evento) por si a alguien le interesa
+        OnNuevaMision.Invoke();
     }
 }
