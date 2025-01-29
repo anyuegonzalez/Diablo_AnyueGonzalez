193206b92bd01bf88aa68dd24f9591dbde46316a
DESKTOP-GV16N45\Alumno <anyuee07@gmail.com>
2025-01-29 20:45:18 +0100

    x

--- Assets/Scripts/Npc.cs
+++ Assets/Scripts/Npc.cs
@@ -6,21 +6,43 @@ using UnityEngine.Rendering;
 
 public class Npc : MonoBehaviour, IInteractuable
 {
+    [SerializeField] private EventManagerSO eventManager;
+
+    [SerializeField] private MisionSO misionAsociada;
+
     private Outline outline;
     [SerializeField] private float tiempoRotacion;
 
     [SerializeField] private DialogaSO dialogo;
+    [SerializeField] private DialogaSO dialogo2;
 
-    [SerializeField] private Transform cameraPoint;
+    private DialogaSO dialogoActual;
+
+    [SerializeField] private Transform puntoCamara;
     private void Awake()
     {
+        dialogoActual = dialogo;
         outline = GetComponent<Outline>();
     }
     // para que el npc se gire hacia el que le esta interactuando (jugador)
     public void Interactuar(Transform interactuador)
     {
-        transform.DOLookAt(interactuador.position, tiempoRotacion, AxisConstraint.Y).OnComplete(()=> SistemaDialogo.trono.IniciarDialogo(dialogo, cameraPoint));
+        transform.DOLookAt(interactuador.position, tiempoRotacion, AxisConstraint.Y).OnComplete(()=> SistemaDialogo.trono.IniciarDialogo(dialogoActual, puntoCamara));
     }
+    private void OnEnable()
+    {
+        // me suscribo al evento para esatr atento de cuando cambiar dialogo
+        eventManager.OnTerminarMision += CambiarDialogo;
+    }
+
+    private void CambiarDialogo(MisionSO misionTerminada)
+    {
+       if(misionTerminada == misionAsociada)
+       {
+            dialogoActual = dialogo2;
+       }
+    }
+
     private void OnMouseEnter()
     {  
         outline.enabled = true;
@@ -29,9 +51,5 @@ public class Npc : MonoBehaviour, IInteractuable
     private void OnMouseExit()
     {
         outline.enabled = false;
-    }
-    private void IniciarDialogo()
-    {
-        //SistemaDialogo.sD.IniciarDialogo(dialogo, punto.Camara);
     }
 }
